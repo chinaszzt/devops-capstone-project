37182a88f389807d41178ae30a72669cd2c4ce83
chinaszzt <chinaszzt@outlook.com>
2022-12-04 10:16:14 -0500

    read accounts

--- service/routes.py
+++ service/routes.py
@@ -68,7 +68,13 @@ def create_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
+@app.route("/accounts/<int:id>", methods=["GET"])
+def read_account(id):
+    account=Account.find(id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND)
+    dic=account.serialize()
+    return dic,status.HTTP_200_OK
 
 
 ######################################################################
